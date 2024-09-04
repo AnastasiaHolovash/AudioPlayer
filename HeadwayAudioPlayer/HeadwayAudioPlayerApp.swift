ee14097a5f9c9d0cb71d4b17dfeeec448e7b0623
Anastasiia Holovash <nastyuha.holovasch@gmail.com>
2024-09-04 20:49:10 +0300

    Initial Commit

---
+++ HeadwayAudioPlayer/HeadwayAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadwayAudioPlayerApp.swift
+//  HeadwayAudioPlayer
+//
+//  Created by Anastasia Holovash on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct HeadwayAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
